dd61fe5cbdf8a4e071eacfec9660cf23c056bb91
milamaranth <mila.sanchezcarrero@gmail.com>
2022-10-05 13:45:51 +0200

    bootcamp eureka server

--- src/main/java/com/nttdata/bootcamp/eureka/ProductServiceEurekaApplication.java
+++ src/main/java/com/nttdata/bootcamp/eureka/ProductServiceEurekaApplication.java
@@ -1,4 +1,4 @@
-package com.nttdata.bootcamp.productservice.eureka;
+package com.nttdata.bootcamp.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
